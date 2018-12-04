ac718ccad97b16e3a52811e4409a6152f2fcddf8
achoory <>
2018-12-04 21:45:10 +0100

    some initial ideas for Q2

---
+++ Day 2/Dec2_Q1.py
@@ -0,0 +1,13 @@
+import collections
+results = collections.Counter("dqddwqfwqfggqwq")
+twoList = []
+threeList = []
+
+for res in results:
+    #print(res, results[res])
+    if (results[res] == 2):
+      print(res, results[res])
+    if (results[res] == 3):
+      print(res, results[res])
+
+#print(res, results[res])
